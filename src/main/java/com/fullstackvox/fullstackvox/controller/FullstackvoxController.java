b7893508531abddc5cb8565904b66194922cf141
edsonfrs <edsonfrs@gmail.com>
2021-05-04 18:39:53 -0300

    Métodos para criação de posts e gravação dos mesmos no banco

--- src/main/java/com/fullstackvox/fullstackvox/controller/FullstackvoxController.java
+++ src/main/java/com/fullstackvox/fullstackvox/controller/FullstackvoxController.java
@@ -1,13 +1,19 @@
 package com.fullstackvox.fullstackvox.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fullstackvox.fullstackvox.model.Post;
 import com.fullstackvox.fullstackvox.service.FullstackvoxService;
@@ -36,8 +42,24 @@ public class FullstackvoxController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+	} 
+		
+		post.setData(LocalDate.now());
+		fullstackvoxService.save(post);
+		return "redirect:/posts";
 
-
+	}
 	
 
 }
